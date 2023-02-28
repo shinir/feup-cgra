6706a7acdf9bc58b27e01b102ca2af1f0d138dc3
201905451 <up201905451@up.pt>
2023-02-28 21:15:47 +0000

    tp4 still with triangles

--- tp2/MyQuad.js
+++ tp2/MyQuad.js
@@ -13,7 +13,7 @@ export class MyQuad extends CGFobject {
 	initBuffers() {
 		this.vertices = [
 			-0.5, -0.5, 0,	//0
-			0.5, -0.5, 0,	//1
+			0.5, -0.5, 0,   //1
 			-0.5, 0.5, 0,	//2
 			0.5, 0.5, 0		//3
 		];
@@ -40,44 +40,41 @@ export class MyUnitQuad extends CGFobject {
 	}
 
     display() {
+
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI,0,1,0);
+        this.scene.translate(-0.5,0.5,0);
         this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(0,0,1);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.rotate(Math.PI/2,0,1,0);
-        this.scene.translate(-0.5,0,-0.5);
+        this.scene.translate(0.5,0.5,1);
         this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI/2,0,1,0);
-        this.scene.translate(-0.5,0,0.5);
+        this.scene.translate(-0.5,0.5,1);
         this.quad.display();
         this.scene.popMatrix();
-
+        
         this.scene.pushMatrix();
         this.scene.rotate(-Math.PI/2,0,1,0);
-        this.scene.translate(0.5,0,0.5);
+        this.scene.translate(0.5,0.5,0);
         this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.rotate(-Math.PI/2,1,0,0);
-        this.scene.translate(0,-0.5,0.5);
+        this.scene.translate(0.5,-0.5,1);
         this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI/2,1,0,0);
-        this.scene.translate(0,0.5,0.5);
+        this.scene.translate(0.5,0.5,0);
         this.quad.display();
         this.scene.popMatrix();
+    
     }
 }
