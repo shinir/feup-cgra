60715b41cbee5dfe52038384cde2dd701cfdadf2
shini <raquelmcarneiro16@gmail.com>
2023-03-14 17:45:55 +0000

    tp3 prisma is done

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -1,43 +1,79 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
- * MyPrism
+ * MyDiamond
  * @constructor
  * @param scene - Reference to MyScene object
  */
 export class MyPrism extends CGFobject {
-	constructor(scene, slices, stacks){
+	constructor(scene, slices, stacks) {
 		super(scene);
-
 		this.slices = slices;
 		this.stacks = stacks;
-
 		this.initBuffers();
 	}
 	
 	initBuffers() {
+
 		this.vertices = [];
+		this.normals = [];
+		var period = 2 * Math.PI / this.slices;
 
- 		this.indices = [];
+		for (var ang = 0; ang < 2 * Math.PI; ang += period){
 
- 		this.normals = [];
+			const x = Math.cos(ang);
+			const y = Math.sin(ang);
 
-		this.angle = Math.PI*2/this.slices;
-	
-		for (j = 0; j < this.stacks; j++) { 
-			for (i = 0; i < this.slices; i++) { 
-				this.vertices.push(Math.cos(this.angle*i),Math.sin(this.angle*i),j/this.stacks);
-				this.vertices.push(Math.cos(this.angle*i),Math.sin(this.angle*i),(j+1)/this.stacks);
-				this.vertices.push(Math.cos(this.angle*(i+1)),Math.sin(this.angle*(i+1)),j/this.stacks);
-				this.vertices.push(Math.cos(this.angle*(i+1)),Math.sin(this.angle*(i+1)),(j+1)/this.stacks);	
-				this.normals.push(Math.cos(this.angle*i+this.angle/2),Math.sin(this.angle*i+this.angle/2),0);
-				this.normals.push(Math.cos(this.angle*i+this.angle/2),Math.sin(this.angle*i+this.angle/2),0);
-				this.normals.push(Math.cos(this.angle*i+this.angle/2),Math.sin(this.angle*i+this.angle/2),0);
-				this.normals.push(Math.cos(this.angle*i+this.angle/2),Math.sin(this.angle*i+this.angle/2),0);
+			for (var s = 0; s <= this.stacks; s++) {
+
+				var z = s * (1/this.stacks);
+
+				this.vertices.push(x, y, z);
+				this.vertices.push(x, y, z);
+
+				var x1 = Math.cos(ang - (period/2));
+				var y1 = Math.sin(ang - (period/2));
+				var z1 = 0; 
+				this.normals.push(x1, y1, z1);
+
+				var x2 = Math.cos(ang + (period/2));
+				var y2 = Math.sin(ang + (period/2));
+				var z2 = 0; 
+				this.normals.push(x2, y2, z2);
+			}			
+		}
+		
+
+		//Counter-clockwise reference of vertices
+		this.indices = [];
+		var space = (this.stacks + 1) * 2;
+
+		for (var level = 0; level < this.stacks; level++) {
+			
+			for (var s = 0; s < this.slices ; s++) {
+
+				var a = 1 + level * 2 + space * s;
+				var b = a + 2;
+				var c = a + space - 1;
+				var d = c + 2;
+
+				a = a >= (this.vertices.length / 3) ? a - this.vertices.length / 3 : a;
+				b = b >= (this.vertices.length / 3) ? b - this.vertices.length / 3 : b;
+				c = c >= (this.vertices.length / 3) ? c - this.vertices.length / 3 : c;
+				d = d >= (this.vertices.length / 3) ? d - this.vertices.length / 3 : d;
+
+				this.indices.push(a, b, c);
+				this.indices.push(a, c, b);
+				
+				this.indices.push(b, c, d);
+				this.indices.push(b, d, c);
+
 			}
 		}
 
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
+
 		this.initGLBuffers();
 	}
 }
-
--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -5,6 +5,7 @@ import { MyPlane } from "./MyPlane.js";
 import { MyTangram } from "./MyTangram.js";
 import { MyUnitCube } from "./MyUnitCube.js";
 import { MyPrism } from "./MyPrism.js";
+import { MyCilinder } from "./MyCilinder.js";
 
 /**
 * MyScene
@@ -35,18 +36,19 @@ export class MyScene extends CGFscene {
         this.pyramid = new MyPyramid(this, 3, 1);
         this.tangram = new MyTangram(this);
         this.cube = new MyUnitCube(this);
-        this.prism = new MyPrism(this);
+        this.prism = new MyPrism(this, 8, 20);
+        this.cilinder = new MyCilinder(this, 8, 1)
         
-        this.objects = [this.plane, this.pyramid, this.cone,this.tangram, this.cube, this.prism];
+        this.objects = [this.plane, this.pyramid, this.cone,this.tangram, this.cube, this.prism, this.cilinder];
 
         // Labels and ID's for object selection on MyInterface
-        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram': 3, 'Cube': 4, 'Prism': 5};
+        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram': 3, 'Cube': 4, 'Prism': 5, 'Cilinder': 6};
 
         //Other variables connected to MyInterface
         this.selectedObject = 0;
         this.selectedMaterial = 0;
         this.displayAxis = true;
-        this.displayNormals = false;
+        this.displayNormals = true;
         this.objectComplexity = 0.5;
         this.scaleFactor = 2.0;
         this.ambientLight = 0.3;
@@ -186,11 +188,12 @@ export class MyScene extends CGFscene {
         this.scale(this.scaleFactor,this.scaleFactor,this.scaleFactor);
         this.updateGlobalAmbientLight();
         
-        if (this.displayNormals)
+        if (this.displayNormals){ 
             this.objects[this.selectedObject].enableNormalViz();
-        else
+        }
+        else {
             this.objects[this.selectedObject].disableNormalViz();
-        
+        }
         this.objects[this.selectedObject].display();
         this.popMatrix();
         // ---- END Primitive drawing section
