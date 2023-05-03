b9cb75844d681e14292d2be412bfc58b5850636b
shini <raquelmcarneiro16@gmail.com>
2023-05-03 09:26:56 +0100

    part 2 done

--- project/MyBird.js
+++ project/MyBird.js
@@ -1,10 +1,5 @@
 import { CGFobject } from "../lib/CGF.js";
-import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
-import { MyPyramid } from "./MyPyramid.js";
-import { MyPrism } from "./MyPrism.js";
-import { MyLeftWing } from "./MyLeftWing.js";
-import { MyRightWing } from "./MyRightWing.js";
-import { MySquare } from "./MySquare.js";
+
 
 
 class MyBird extends CGFobject {
